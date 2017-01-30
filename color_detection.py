44a06ba9eb1250c27531cb31dc23f94458091795
Ebey Abraham <ebey97@gmail.com>
2017-01-30 18:53:03 +0530

    Create color_detection.py
    initial commit

---
+++ color_detection.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+cap=cv2.VideoCapture(0)
+
+while(cap.isOpened()):
+    ret,img=cap.read()
+    #BGR Image
+    cv2.imshow('Capturing...',img)
+    hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    #Binary
+    msk=cv2.inRange(hsv,np.array([170,170,10]),np.array([200,200,200]))
+    res=cv2.bitwise_and(img,img,mask=msk)
+    cv2.imshow('Result',res)
+    k=cv2.waitKey(10)
+    if k==27:
+        break
+cap.release()
+cv2.destroyAllWindows()
